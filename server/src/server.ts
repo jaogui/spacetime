f1aaf293fa108286710b93b13d5673aa9ef49c46
João Guilherme <joaobenine@gmail.com>
2023-05-15 21:04:46 -0300

    server config funfando

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log('oi');
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', ()=>{
+    return 'hello'
+
+})
+
+//Inicializando Server na porta 3333
+app.listen({
+    port: 3333,
+}).then(() =>{
+    console.log('Server rodando em localhost:3333 👌');
+
+})

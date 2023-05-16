836408efc8dffe46d9aeb310a782048ec36b119b
João Guilherme <joaobenine@gmail.com>
2023-05-16 00:28:50 -0300

    install config eslint

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className='bg-slate-500 flex-1'>
-      <Text className='bg-black'>Hello word</Text>
-      <StatusBar style="auto" />
+    <View className="bg-slate-500 flex-1">
+      <Text className="bg-black">Hello word</Text>
+      <StatusBar style="light" />
     </View>
-  );
+  )
 }

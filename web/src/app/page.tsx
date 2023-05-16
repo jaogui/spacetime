01a13dec317fb1db6084d2bba60946b798c91804
João Guilherme <joaobenine@gmail.com>
2023-05-16 20:36:05 -0300

    add background home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+    <main className="grid min-h-screen grid-cols-2 bg-[url('../assets/bg-stars.svg')] bg-cover">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full " />
 
-        <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes"></div>
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
       </div>
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">

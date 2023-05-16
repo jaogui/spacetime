49ae7b9150b8af701899d5a24a6951ea5cd5a17c
João Guilherme <joaobenine@gmail.com>
2023-05-15 21:53:02 -0300

    install slint config @rocketseat

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,13 @@
-import fastify from 'fastify';
-
-const app = fastify();
-
-app.get('/hello', ()=>{
-    return 'hello'
-
+import fastify from 'fastify'
+const app = fastify()
+app.post('/hello', () => {
+  return 'hello'
 })
-
-//Inicializando Server na porta 3333
-app.listen({
+// Inicializando Server na porta 3333
+app
+  .listen({
     port: 3333,
-}).then(() =>{
-    console.log('Server rodando em localhost:3333 👌');
-
-})
+  })
+  .then(() => {
+    console.log('Server rodando em localhost:3333 👌')
+  })

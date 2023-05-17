c90410c8b114cf4d9063c25055f3eeb8050785c2
João Guilherme <joaobenine@gmail.com>
2023-05-17 00:17:53 -0300

    Estrutura e estilos page inicial mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree/'
 import { styled } from 'nativewind'
 
 import blurBg from './src/assets/luz.png'
 import Stripes from './src/assets/stripes.svg'
+import LogoMobile from './src/assets/logo-mobile.svg'
 
 
-const StyledStripes = styled(Stripes) 
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -24,10 +25,30 @@ export default function App() {
 
   return (
     <ImageBackground source={blurBg}
-      className="bg-gray-900 flex-1 items-center relative"
-      imageStyle={{position: 'absolute', left: '-100%'}}>
+      className="bg-gray-900 flex-1 items-center relative px-10 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}>
 
-        <StyledStripes className="absolute left-0" />
+      <StyledStripes className="absolute left-0 top-10" />
+
+      <View className="flex-1  items-center justify-center gap-6">
+        <LogoMobile />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          className="rounded-full bg-purple-400 px-5 py-3"
+          activeOpacity={0.7}>
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastre-se lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-gray-100 text-center font-body text-sm leading-relaxed">joãoGui feat Rocketseat 💜</Text>
       <StatusBar style="light" />
     </ImageBackground>
   )

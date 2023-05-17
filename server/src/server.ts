ed806e7b9f1c4615e346e855aa2ad683587e9b2e
João Guilherme <joaobenine@gmail.com>
2023-05-17 00:58:31 -0300

    organizando files p/ iniciar rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,8 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
+import { memoriesRoutes } from './routes/memories'
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
-// Inicializando Server na porta 3333
+app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,

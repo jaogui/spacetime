94efbb48f93cc063de198e268cdae1884040acf5
João Guilherme <joaobenine@gmail.com>
2023-05-17 01:21:44 -0300

    rota p/ buscar memorias filtrando caracteres do texto

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -2,9 +2,27 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
+  // Busca Memorias
   app.get('/memories', async () => {
-    const users = await prisma.user.findMany()
-
-    return users
+    const memories = await prisma.memoryPost.findMany({
+      orderBy: {
+        createAt: 'asc',
+      },
+    })
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        imgUrl: memory.imgUrl,
+        excerpt: memory.contentText.substring(0, 115).concat('...'),
+      }
+    })
   })
+  // Busca Memorias pelo id
+  app.get('/memories:id', async () => {})
+  // Cria Memoria
+  app.post('/memories', async () => {})
+  // Atualiza Memoria
+  app.put('/memories:id', async () => {})
+  // Remove MemorIa
+  app.delete('/memories:id', async () => {})
 }

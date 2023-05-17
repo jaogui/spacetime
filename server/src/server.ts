6ba3a623a2e144543d0de24d2f1bd2bfbb377036
João Guilherme <joaobenine@gmail.com>
2023-05-17 01:56:55 -0300

    instal fastify/cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // Tds urls front-end poderam acessar o back-end
+})
 app.register(memoriesRoutes)
 app
   .listen({

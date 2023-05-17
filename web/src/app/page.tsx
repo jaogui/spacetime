d596491528cbbc3fcd6d386e6e83db02ffd0a993
João Guilherme <joaobenine@gmail.com>
2023-05-16 22:05:33 -0300

    content sign-in + pacote p/ iconeslucide

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { User } from 'lucide-react'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2 bg-[url('../assets/bg-stars.svg')] bg-cover">
@@ -5,6 +7,21 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full " />
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-100"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">
+              {' '}
+              Crie sua conta e salve suas memórias!
+            </span>
+          </p>
+        </a>
       </div>
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">

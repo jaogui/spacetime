97cf548447230b9a701432855117d25b495a2122
João Guilherme <joaobenine@gmail.com>
2023-05-16 23:33:06 -0300

    config colors + fontes tailwind mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold} from '@expo-google-fonts/bai-jamjuree/'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    BaiJamjuree_700Bold,
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  //Só carrega app depois das fontes
+  if(!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-slate-500 flex-1">
-      <Text className="bg-black">Hello word</Text>
+    <View className="bg-gray-400 flex-1 items-center justify-center">
+      <Text className="text-lg font-alt text-gray-50">Hello Big</Text>
       <StatusBar style="light" />
     </View>
   )

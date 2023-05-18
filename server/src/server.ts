c4170fe177c3d0086c27c445416db0098c7a53bf
João Guilherme <joaobenine@gmail.com>
2023-05-17 23:45:15 -0300

    init rota p/ usuario se cadastrar/logar github

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 const app = fastify()
 
 app.register(cors, {
   origin: true, // Tds urls front-end poderam acessar o back-end
 })
 app.register(memoriesRoutes)
+app.register(authRoutes)
 app
   .listen({
     port: 3333,

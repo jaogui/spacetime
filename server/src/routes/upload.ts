222391ad218d519c4b379a880fc32e01afc85a0c
João Guilherme <joaobenine@gmail.com>
2023-05-21 01:00:41 -0300

    rota p/ upload de arquivo

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,4 +1,11 @@
+import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'fs'
+import { extname, resolve } from 'path'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+
+const pump = promisify(pipeline)
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
@@ -12,7 +19,7 @@ export async function uploadRoutes(app: FastifyInstance) {
       return reply.status(400).send()
     }
 
-    const mimeTypeRegex = /^(image|video)?[a-zA-Z]+/
+    const mimeTypeRegex = /\.(jpg|jpeg|png|gif|mp4|avi|mov)$/i
     const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
 
     if (!isValidFileFormat) {
@@ -20,5 +27,19 @@ export async function uploadRoutes(app: FastifyInstance) {
     }
 
     console.log(upload.file)
+
+    const fieldId = randomUUID()
+    const extensionFile = extname(upload.filename)
+    const fileName = fieldId.concat(extensionFile)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads/', fileName),
+    )
+    await pump(upload.file, writeStream)
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname)
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return { fileUrl }
   })
 }

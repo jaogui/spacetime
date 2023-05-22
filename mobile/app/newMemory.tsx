1d9f216035325b53d2d5ff25797777afca437d40
João Guilherme <joaobenine@gmail.com>
2023-05-22 00:38:34 -0300

    preview img mobile - expo image picker

--- mobile/app/newMemory.tsx
+++ mobile/app/newMemory.tsx
@@ -1,18 +1,35 @@
-import { ScrollView, Switch, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Switch, Text, TextInput, TouchableOpacity, View , Image} from 'react-native'
 import SpaceTimeLogo from '../src/assets/logo-mobile.svg'
 import { Link } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
 import React, { useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as ImagePicker from 'expo-image-picker';
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const [preview, setPreview] = useState<string | null>(null)
   const [contentText, setContentText] = useState('')
   const [isPublic, setIsPublic] = useState(false)
 
 
-  function openImagePicker() {
+  async function openImagePicker() {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      });
+      if(result.assets[0]){
+          setPreview(result.assets[0].uri)
+      }
+    }catch(err){
+      //erro não tratado
+    }
 
+    //   if (!result.canceled) {
+    //     setImage(result.assets[0].uri);
+    //   }
+    // };
   }
   function handleCreateMemory() {
     console.log(contentText, isPublic)
@@ -46,12 +63,16 @@ export default function NewMemory() {
           activeOpacity={0.7}
           className="h-32 justify-center items-center rounded-lg border-dashed border-gray-500
          bg-black/20">
-          <View className="flex-row items-center gap-2">
+          {preview ? (
+            <Image source={{uri: preview}} className="h-full w-full rounded-lg object-cover" />
+          ) : (
+            <View className="flex-row items-center gap-2">
             <Icon name="image" color="#FFF" />
             <Text className='font-body text-sm text-gray-200 items-center'>
               Adicionar Foto ou video de capa
             </Text>
           </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
